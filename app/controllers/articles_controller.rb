0af2a96b6c56d06422de4611caae43af4ef5c2f9
Tim Flood <tflood9@gmail.com>
2021-09-14 17:01:19 -0400

    added basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "user", password: "password",
+  except: [:index, :show]
+
   def index
     @articles = Article.all
   end
